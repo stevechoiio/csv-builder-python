d3cedb97ecb252f200d09f66c1464fb70fe23c8a
Steve Choi <stevechoi93@gmail.com>
2019-03-06 22:37:08 -0800

    created simpler test3, converted to dictionary

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-with open('test3.csv','r') as test3:
-    test = test3.read()
-    (test,...a) = test.splitlines()
-    print(test)
-    print(a)
+with open('test3.csv', 'r') as test3:
+    obj = []
 
+    test = test3.read()
+
+    test = test.splitlines()
+    a, *b = test
+    c, *d = b
+    # print(a)
+    key = a.split(',')
+    resultObj = {}
+    result = c.split(',')
+
+    resultObj = dict(zip(key, result))
+    print(resultObj)
+    # resultObj[x] = result[index]
+    # print(resultObj)
+    # b.split(',')
+    # print(b)
