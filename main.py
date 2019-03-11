d26f1bc80eadfb66f8333e07f7a7268ebb6056f2
Steve Choi <stevechoi93@gmail.com>
2019-03-10 22:22:28 -0700

    parsing simple tests #3 and #4 done

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import re
-with open('test3.csv', 'r') as test:
+with open('test4.csv', 'r') as test:
     obj = []
 
     csv = test.read()
 
     key, info = csv.split('\n', 1)
     key = key.split(',')
-    # print(info)
+
     parsedInfo = re.findall(
         '[a-zA-Z0-9\s:]+|"[\s0-9a-zA-Z,]+"', info)
-
     print(len(key))
-
     newInfo = []
     for i in parsedInfo:
         newInfo += (i.split('\r\n'))
